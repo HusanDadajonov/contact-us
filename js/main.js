5261cf74a6e7eaf8af03ce880253a21d37bf6eb2
am1rcek <xusandadajonov4385@gmail.com>
2021-12-06 17:03:35 +0500

    awdaw

--- js/main.js
+++ js/main.js
@@ -8,43 +8,67 @@ let elFamilyBtn = document.querySelector(".family-btn");
 let elFriendsBtn = document.querySelector(".friends-btn");
 let elClassMate = document.querySelector(".classmate-btn");
 let elAllBtn = document.querySelector(".all-btn");  
+let elSearchInp = document.querySelector(".search");
 let arr = [];
+let namesArr = [];
 let obj = {};
 
 elForm.addEventListener("submit", send);
+
+elSearchInp.addEventListener("keyup", search);
+
+function search(e){
+    let elInpValue = e.target.value.toLowerCase();
+    
+    namesArr.forEach(item =>{
+        let userName = item.textContent.toLowerCase();
+
+        if(!userName.includes(elInpValue)){
+            item.parentElement.style.display = "none";
+        }
+        else{
+            item.parentElement.style.display = "block";
+        }
+    })
+}
 
 function createElement(element){
     elList.innerHTML = "";
     element.forEach(item =>{
         let elLi = document.createElement("li");
-        let elH3 = document.createElement("h3");
-        elH3.innerHTML = item.fullname;
-        elH3.className = "box__name";
         elLi.className = "box__item border mb-3 p-3 rounded";
-        elLi.innerHTML = `<h3 class="box__name">${item.fullname}</h3><p class="box__relationShip">${item.relationship}</p><a class="box__tell btn btn-primary text-decoration-none fw-bold" href = ${item.phone}>${item.phone}</a>`;
+        let name = document.createElement("h3");
+        name.className = "box__name";
+        name.innerHTML = item.fullname;
+        let relationship = document.createElement("p");
+        relationship.className = "box__relationShip";
+        relationship.innerHTML = item.relationship;
+        let tel = document.createElement("a");
+        tel.className = "box__tell btn btn-primary text-decoration-none fw-bold";
+        tel.href = `${item.phone}`
+        tel.innerHTML = item.phone;
+        elLi.appendChild(name);
+        elLi.appendChild(relationship);
+        elLi.appendChild(tel);
         elList.append(elLi);
+        namesArr.push(name);
     }) 
 }
 
 function send (e) {
     e.preventDefault();
-    if(obj.phone != elPhoneInp.value){
-        if(elNameInp.value != "" && elRealtionInp.value != "" && elPhoneInp.value != ""){
-            obj = {
-                fullname : elNameInp.value,
-                relationship : elRealtionInp.value,
-                phone : elPhoneInp.value
-            };
-            arr.push(obj);
-            createElement(arr);
-        }
+    let userPhone = arr.filter(item => item.phone === elPhoneInp.value);
+    console.log(userPhone);
+    if(elNameInp.value != "" && elRealtionInp.value != "" && elPhoneInp.value != "" && userPhone.length === 0){
+        obj = {
+            fullname : elNameInp.value,
+            relationship : elRealtionInp.value,
+            phone : elPhoneInp.value
+        };
+        arr.push(obj);
+        createElement(arr);
     }
-    else elPhoneInp.className = "error-inp inputs inp-phone form-control";
     elAllInputs.forEach(item => item.value = "");
-}
-
-function sortFamily(arrays){
-    console.log(arrays);
 }
 
 elFamilyBtn.addEventListener("click", (e) =>{
@@ -83,3 +107,6 @@ elClassMate.addEventListener("click", (e) =>{
 elAllBtn.addEventListener("click", (e) =>{
     createElement(arr);
 })
+
+
+
